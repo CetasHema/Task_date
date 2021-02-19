2e91bd982dfcb1ba3e6cb5c40d9a2a7215d7d1b6
Hemakumar.V <Hemakumar.v@cetastech.com>
2021-02-19 12:35:57 +0530

    newcomit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,8 +6,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            DateTime dt = new DateTime();
-            var date = DateTime.Now;
+          
+            
+            DateTime date = DateTime.Now;
+            Console.WriteLine("Curent date and time:" + date);
             var date1 = $"{date:d}";
             Console.WriteLine(date.ToString("dd"));
             Console.WriteLine(date.ToString("MM"));
@@ -16,41 +18,42 @@ namespace ConsoleApp1
             Console.WriteLine(wk);
             
             //Add Days
-            Console.WriteLine("Enter the number of days to add :");
+            Console.WriteLine("\nEnter the number of days to add :");
             int days = Convert.ToInt32(Console.ReadLine());
             DateTime newDated = date.AddDays(days);
             Console.WriteLine("Date after "+ days+" days :"+newDated.ToShortDateString());
             
             //Add weeks
-            Console.WriteLine("Enter the number of weeks to add :");
+            Console.WriteLine("\nEnter the number of weeks to add :");
             int weeks = Convert.ToInt32(Console.ReadLine());
             DateTime newDatew = date.AddDays(weeks*7);
             Console.WriteLine("Date after " + days + " days :" + newDatew.ToShortDateString());
             
             //Add months
-            Console.WriteLine("Enter the number of months to add :");
+            Console.WriteLine("\nEnter the number of months to add :");
             int months = Convert.ToInt32(Console.ReadLine());
             DateTime newDatem = date.AddMonths(months);
             Console.WriteLine(newDatem.ToShortDateString());
 
             //Add Years
-            Console.WriteLine("Enter the number of years to add :");
+            Console.WriteLine("\nEnter the number of years to add :");
             int years = Convert.ToInt32(Console.ReadLine());
             DateTime newDatey = date.AddYears(years);
             Console.WriteLine(newDatey.ToShortDateString());
 
-            Console.WriteLine("Adding time......");
+            //Add Time
+            Console.WriteLine("\n\n......Adding time......");
 
-            Console.WriteLine("Enter the number of hours to add :");
+            Console.WriteLine("\nEnter the number of hours to add :");
             int hr = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the number of minutes to add :");
             int mnt = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the number of seconds to add :");
             int sec = Convert.ToInt32(Console.ReadLine());
             TimeSpan ts = new TimeSpan(hr,mnt,sec);
-            DateTime dt1 = dt.Add(ts);
-            Console.WriteLine("Curent date and time:" + dt.ToShortDateString());
-            Console.WriteLine("Date after adding time:" + dt1.ToShortDateString());
+            DateTime dt1 = date.Add(ts);
+            Console.WriteLine("\nCurent date and time:" + date);
+            Console.WriteLine("Date after adding time:" + dt1);
 
 
 
